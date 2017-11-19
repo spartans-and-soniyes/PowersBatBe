fef37f40112ea638320bba9fe58b394b242fe9e1
Shishir Jakati <shishir.jakati98@gmail.com>
2017-11-18 19:26:53 -0500

    Get camera to work first

--- App.js
+++ App.js
@@ -8,31 +8,45 @@ import {
 } from 'react-native';
 import Camera from "react-native-camera";
 
-/*const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});*/
-
 export default class App extends Component {
-  render() {
-    return (
-      <View >
-        <Camera style={{flex: 1}}
-            ref={cam => this.camera=cam}
-            aspect={Camera.constants.Aspect.fill}>
+  render(){
+    return (<View style={styles.container}>
+        <Camera
+          ref={(cam) => {
+            this.camera = cam;
+          }}
+          style={styles.preview}
+          aspect={Camera.constants.Aspect.fill}>
+          <Text style={styles.capture} onPress={this.takePicture.bind(this)}>[CAPTURE]</Text>
         </Camera>
-      </View>
-    );
+      </View>);
   }
-  /*takePicture() {
+  takePicture() {
     const options = {};
     //options.location = ...
     this.camera.capture({metadata: options})
       .then((data) => console.log(data))
       .catch(err => console.error(err));
-  }*/
+  }
 }
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'row',
+  },
+  preview: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    alignItems: 'center'
+  },
+  capture: {
+    flex: 0,
+    backgroundColor: '#fff',
+    borderRadius: 5,
+    color: '#000',
+    padding: 10,
+    margin: 40
+  }
+});
 
